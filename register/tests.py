fb163dc836fcfe50e06e058b8bd0199f2144e798
moiyajosephs <josephsmoiya@gmail.com>
2020-11-09 01:34:36 -0500

    Update tests.py
    fixed syntax

--- register/tests.py
+++ register/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
+from django.urls import resolve, reverse
+from .views import register
 
 # Create your tests here.
 from django.contrib.auth.models import User
 class SignUpTests(TestCase):
     def test_signup_status_code(self):
-        url = reverse('signup')
+        url = reverse('register')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
     def test_signup_url_resolves_signup_view(self):
-        view = resolve('/signup/')
-        self.assertEquals(view.func, signup)
+        view = resolve('/register/')
+        self.assertEquals(view.func, register)
