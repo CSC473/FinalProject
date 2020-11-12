c9d9f5df47e786f8fc504d3fc469a91837015621
Moiya Josephs <49174334+moiyajosephs@users.noreply.github.com>
2020-11-11 21:16:54 -0500

    Update models.py

--- calendar_monthly_view/models.py
+++ calendar_monthly_view/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from django.contrib.auth.models import User
 
 class Event(models.Model):
-    user = models.ForeignKey(User, default="1", on_delete= models.CASCADE)
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
     end_time = models.DateTimeField()
