a332df3b5abd2dc14f5c6fefdf71498a41ec505c
moiyajosephs <josephsmoiya@gmail.com>
2020-11-11 21:22:45 -0500

    try to fix db

--- register/migrations/0001_initial.py
+++ register/migrations/0001_initial.py
@@ -15,15 +15,10 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Event',
+            name='UserProfile',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=200)),
-                ('description', models.TextField()),
-                ('end_time', models.DateTimeField()),
-                ('start_time', models.DateTimeField()),
-                ('completed', models.BooleanField(default=False)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
