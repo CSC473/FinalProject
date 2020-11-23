db73e151347d558c4b611f49e8ab399930a93336
Maliha Tabassum <tabassummaliha006@gmail.com>
2020-11-22 23:33:51 -0500

    register tests

--- register/tests.py
+++ register/tests.py
@@ -1,14 +1,40 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
-from .views import register
-
-# Create your tests here.
 from django.contrib.auth.models import User
+from .views import register
+from register.forms import RegisterForm
+from register.models import UserProfile
+from django.contrib import auth
+
+class RegisterFormTest(TestCase):
+    def test_registration_success(self):
+        form = RegisterForm({
+            'username': 'testingcode',
+            'email': 'testing@gmail.com',
+            'password1' : "error777",
+            'password2': "error777",
+        })
+        self.assertTrue(form.is_valid())
+        res = form.save()
+        assert res.is_active, "registration failed"
+
+
+    def test_registration_fail(self):
+        form = RegisterForm({
+            'username': 'testingcode',
+            'email': 'testing',
+            'password1' : "error777",
+            'password2': "error777",
+        })
+        self.assertFalse(form.is_valid())
+
+
 class SignUpTests(TestCase):
     def test_signup_status_code(self):
         url = reverse('register')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
+    
     def test_signup_url_resolves_signup_view(self):
         view = resolve('/register/')
         self.assertEquals(view.func, register)
